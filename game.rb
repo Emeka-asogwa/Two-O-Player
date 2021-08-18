192636ae447fcbbe9b9fe1eb0b328d5b93136223
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-08-18 14:43:50 -0400

    ✍🏽 Add games

--- game.rb
+++ game.rb
@@ -9,3 +9,29 @@ class Game
     @players << Player.new(2)
     @turn = 0
   end
+
+  def start 
+    while !@game_over do 
+      currentPlayer = @players[@turn % 2]
+      otherPlayer = @players[(@turn + 1) % 2]
+      currentQuestion = Question.new(currentPlayer.name)
+      currentQuestion.ask
+      answer = gets.chomp.to_i
+      if currentQuestion.confirm(answer)
+        puts "Correct!"
+      else
+        puts "Not correct"
+        currentPlayer.minusScore
+        if currentPlayer.score == 0
+          puts "#{otherPlayer.name} wins with a score of #{otherPlayer.score}/3"
+          puts "----------Game over---------"
+          puts "Good bye!"
+          break
+        end
+      end
+      puts "P1: #{@players[0].score}/3 vs P2: #{@players[1].score}/3"
+      puts "------- New Game ------"
+      @turn = @turn + 1
+    end
+  end
+end
